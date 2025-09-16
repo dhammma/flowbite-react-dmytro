06de5378c272b1b19b4c2ada45e7fd63e2039595
Dmytro Salenko <salenko.d@gmail.com>
2025-09-17 00:41:53 +0300

    added compare

--- apps/web/components/color-map.tsx
+++ apps/web/components/color-map.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Fragment, useState } from "react";
+import { RangeSlider } from "flowbite-react";
+import { Fragment, useState, type ChangeEvent } from "react";
 import { ColorSourceDropdown, type ColorSource } from "~/components/color-source-dropdown";
 import { colorSources } from "./color-sources";
 
@@ -8,13 +9,39 @@ const SHADES = ["50", "100", "200", "300", "400", "500", "600", "700", "800", "9
 
 export const ColorMap = () => {
   const [selectedSource, setSelectedSource] = useState<ColorSource>("misolla1");
+  const [compareWith, setCompareWith] = useState<ColorSource>("tailwind4");
+  const [offsetCompare, setOffsetCompare] = useState(0);
 
-  return (
-    <div>
-      <div className="mb-4 flex items-center gap-2">
+  const handleOffsetCompareChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setOffsetCompare(Number(event.target.value));
+  };
+
+  const configure = (
+    <>
+      <div className="flex items-center gap-2">
         <span>Color source:</span>
         <ColorSourceDropdown selectedSource={selectedSource} onSourceChange={setSelectedSource} />
       </div>
+      <div className="flex flex-1 items-center gap-2">
+        <RangeSlider
+          className="w-full"
+          value={offsetCompare}
+          onChange={handleOffsetCompareChange}
+          min={0}
+          max={1}
+          step={0.05}
+        />
+      </div>
+      <div className="flex items-center gap-2">
+        <span>Compare with:</span>
+        <ColorSourceDropdown selectedSource={compareWith} onSourceChange={setCompareWith} />
+      </div>
+    </>
+  );
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-2">{configure}</div>
 
       <div className="grid gap-2" style={{ gridTemplateColumns: "min-content 1fr" }}>
         {Object.entries(colorSources).map(([colorName, colorConfig]) => (
@@ -26,31 +53,49 @@ export const ColorMap = () => {
             <div className="flex items-center text-xs font-normal text-gray-200 sm:hidden">{colorName}</div>
             <div className="grid grid-cols-11 gap-1">
               {SHADES.map((shade) => {
-                const hasColor = colorConfig?.[selectedSource]?.[shade];
-
-                if (!hasColor) {
-                  return (
-                    <div key={shade} className="relative aspect-square rounded border border-gray-300">
-                      <div className="absolute inset-0 flex items-center justify-center">
-                        <div className="h-0.5 w-full origin-center rotate-45 transform bg-red-500"></div>
-                        <div className="absolute h-0.5 w-full origin-center -rotate-45 transform bg-red-500"></div>
-                      </div>
-                    </div>
-                  );
-                }
+                const color = colorConfig?.[selectedSource]?.[shade];
+                const compareWithColor = colorConfig?.[compareWith]?.[shade];
 
                 return (
-                  <div
-                    key={shade}
-                    className="aspect-square rounded border border-gray-300"
-                    style={{ backgroundColor: colorConfig?.[selectedSource]?.[shade] ?? "#000000" }}
-                  />
+                  <div key={shade} className="relative aspect-square rounded border border-gray-300">
+                    {!color && <EmptyCell opacity={1 - offsetCompare} />}
+                    {color && <FilledCell color={color} opacity={1 - offsetCompare} />}
+                    {!compareWithColor && <EmptyCell opacity={offsetCompare} />}
+                    {compareWithColor && <FilledCell color={compareWithColor} opacity={offsetCompare} />}
+                  </div>
                 );
               })}
             </div>
           </Fragment>
         ))}
       </div>
+      <div className="mt-4 flex items-center gap-2">{configure}</div>
     </div>
+  );
+};
+
+type CellProps = {
+  opacity: number;
+};
+
+const EmptyCell = ({ opacity }: CellProps) => {
+  return (
+    <div className="absolute inset-0 flex h-full w-full items-center justify-center" style={{ opacity }}>
+      <div className="h-0.5 w-full origin-center rotate-45 transform bg-red-500"></div>
+      <div className="absolute h-0.5 w-full origin-center -rotate-45 transform bg-red-500"></div>
+    </div>
+  );
+};
+
+type FilledCellProps = CellProps & {
+  color: string;
+};
+
+const FilledCell = ({ color, opacity }: FilledCellProps) => {
+  return (
+    <div
+      className="absolute inset-0 flex h-full w-full items-center justify-center"
+      style={{ opacity, backgroundColor: color }}
+    ></div>
   );
 };
